c2f3bcd5f0492db6d77ff016bf36fa8e2a37ed14
sithumonline <sithumonline@users.noreply.github.com>
2021-10-04 06:16:14 +0000

    deploy: b3cbc58139fe5801460337648280bb61acce478d

---
+++ _next/static/Re20ZFpRg0gslh_8a9FlB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/85b124a3a9fbccb373fc.css","static/chunks/240-cf0820b2c5fb080fc706.js","static/chunks/pages/index-46f1810a622e9e5405ff.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
