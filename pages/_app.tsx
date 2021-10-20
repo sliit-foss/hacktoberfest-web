c6c3300f1e1ad699aadb86272a7a5a676b02c8bc
Akalanka Perera <akalankaperera128@gmail.com>
2021-10-20 13:44:06 +0530

    Hotfix/aos-refresh

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
+import { useEffect } from 'react'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    Aos.init({ offset: 0, duration: 1200 })
+    if(process.browser){
+      window.addEventListener('load', Aos.refresh)
+    }
+  }, [])
   return <Component {...pageProps} />
 }
 export default MyApp
