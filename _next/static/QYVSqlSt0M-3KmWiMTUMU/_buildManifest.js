4091e4559fc51d8683316aa6ef6a5f667ecc128b
Shehanka <Shehanka@users.noreply.github.com>
2021-10-20 23:06:02 +0000

    deploy: ad718227334e69805db0f21ff5cc1dc15bc1f640

---
+++ _next/static/QYVSqlSt0M-3KmWiMTUMU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/946-f33e1058b98023c052f2.js","static/chunks/pages/index-13fb1896551b19c5576d.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
